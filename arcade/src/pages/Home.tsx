18815ab4405195e6a8c5fed66847ab46a3d643c6
Jek PC <jackpielstick@gmail.com>
2021-06-29 16:41:48 -0400

    create first page

--- arcade/src/pages/Home.tsx
+++ arcade/src/pages/Home.tsx
@@ -1,22 +1,41 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonList, IonItem, IonThumbnail, IonLabel, IonImg } from '@ionic/react';
 import './Home.css';
+
+
+// START of copy-pasted code from ionimg page
+type Item = {
+  src: string;
+  text: string;
+};
+const items: Item[] = [{ src: 'http://placekitten.com/g/200/300', text: 'a picture of a cat' },
+                       { src: 'http://placekitten.com/g/200/300', text: 'a picture of another cat' },
+                       { src: 'http://placekitten.com/g/200/300', text: 'a picture of yet another cat' },];
+//END of this block of copypasta
 
 const Home: React.FC = () => {
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Gamer gaming moment</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">IOS Title / condensed</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonList>
+          {items.map((image, i) => (
+            <IonItem key={i}>
+              <IonThumbnail slot="start">
+                <IonImg src={image.src} />
+            </IonThumbnail>
+            <IonLabel>{image.text}</IonLabel>
+            </IonItem>
+      ))}
+    </IonList>
       </IonContent>
     </IonPage>
   );
